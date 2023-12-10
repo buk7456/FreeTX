0627b819ce2fbdfd17f0732fc461b19e5ac97926
BUK <60001726+buk7456@users.noreply.github.com>
2023-12-10 18:58:41 +0300

    Update templates.h

--- source code/transmitter/mtx/src/templates.h
+++ source code/transmitter/mtx/src/templates.h
@@ -9,11 +9,8 @@ void loadMixerTemplateDiffThrust(uint8_t mixIdx);
 void loadSensorTemplateExtVolts2S(uint8_t telemIdx);
 void loadSensorTemplateExtVolts3S(uint8_t telemIdx);
 void loadSensorTemplateExtVolts4S(uint8_t telemIdx);
-void loadSensorTemplateRPM2Blades(uint8_t telemIdx);
-void loadSensorTemplateRPM3Blades(uint8_t telemIdx);
-void loadSensorTemplateRPM4Blades(uint8_t telemIdx);
-void loadSensorTemplateTemperature(uint8_t telemIdx);
 void loadSensorTemplateRSSI(uint8_t telemIdx);
+void loadSensorTemplateLinkQuality(uint8_t telemIdx);
 
 uint8_t getChannelIdx(char c);
 
