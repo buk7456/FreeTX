f495702bcb9439da089a32866f56d9d5850ed855
BUK <60001726+buk7456@users.noreply.github.com>
2023-12-26 16:17:33 +0300

    Update mtx.ino

--- source code/transmitter/mtx/mtx.ino
+++ source code/transmitter/mtx/mtx.ino
@@ -1,7 +1,8 @@
 /***************************************************************************************************
-  Firmware for the rc transmitter.
+  Firmware for the rc transmitter
   Target mcu: Atmega2560
-  Compiles on Arduino 1.8.x or later
+  Compiles on Arduino 1.8.x or later.
+  Use compiler optimization option -O2 or -O3 if possible.
   
   Authors
   buk7456 <buk7456@gmail.com>
