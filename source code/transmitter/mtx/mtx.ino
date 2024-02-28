51acc002c3fd52e72c0290328b060caabaebfb56
BUK <60001726+buk7456@users.noreply.github.com>
2024-02-28 15:06:10 +0300

    Update mtx.ino

--- source code/transmitter/mtx/mtx.ino
+++ source code/transmitter/mtx/mtx.ino
@@ -2,7 +2,9 @@
   Firmware for the rc transmitter
   Target mcu: Atmega2560
   Compiles on Arduino 1.8.x or later.
-  Use compiler optimization option -O2 or -O3 if possible.
+  - Use compiler optimization option -O2 or -O3 if possible.
+  - The warning "Low memory available, stability problems may occur" can be ignored as long as
+    there is at least 800 bytes available for local variables.
   
   Authors
   buk7456 <buk7456@gmail.com>
