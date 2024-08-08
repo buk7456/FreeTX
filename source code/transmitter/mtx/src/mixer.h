f87ded8cfc985e634ff4caed02e641a67a8eca7e
Buk <buk7456@gmail.com>
2024-08-08 13:21:58 +0300

    Update mixer.h

--- source code/transmitter/mtx/src/mixer.h
+++ source code/transmitter/mtx/src/mixer.h
@@ -5,9 +5,9 @@ void computeChannelOutputs();
 
 int16_t calcRateExpo(int16_t input, int16_t rate, int16_t expo);
 bool checkSwitchCondition(uint8_t sw);
-void moveLogicalSwitch(uint8_t newPos, uint8_t oldPos);
 void moveMix(uint8_t newPos, uint8_t oldPos);
 void swapMix(uint8_t posA, uint8_t posB);
+bool moveLogicalSwitch(uint8_t newPos, uint8_t oldPos);
 void syncWaveform(uint8_t idx);
 void reinitialiseMixerCalculations();
 int16_t adjustTrim(uint8_t idx, int16_t val, uint8_t incButton, uint8_t decButton);
