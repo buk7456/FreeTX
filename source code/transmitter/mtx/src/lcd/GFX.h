06982e73851741a8f68a74b59e1874a0b821668b
Buk <buk7456@gmail.com>
2024-09-29 17:08:15 +0300

    Update GFX.h
    Added license information

--- source code/transmitter/mtx/src/lcd/GFX.h
+++ source code/transmitter/mtx/src/lcd/GFX.h
@@ -1,3 +1,40 @@
+/*
+  Adapted by Buk, from Adafruit GFX lib.
+  
+  This library has been tailored to this project. Some features have been removed or 
+  modified for lower flash usage, reduced overhead, and improved speed.
+  Major changes made:
+    - Using uint8_t types where necessary.
+    - Reimplemented drawHLine, drawVLine, drawRect, fillRect
+    - Removed unused features like custom font support, Xbitmaps, etc.
+    - Changed some member functions to be overridable.
+    
+  Original copyright notice
+  --------------------------
+  Copyright (c) 2013 Adafruit Industries.  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+  - Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+  - Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+*/
+
 #ifndef _GFX_H
 #define _GFX_H
 
