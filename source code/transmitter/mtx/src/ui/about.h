c7a391c0952324baca7a38740ee140ffeb666c93
Buk <buk7456@gmail.com>
2024-10-24 17:08:16 +0300

    Update about.h
    Added support for page break (form feed)

--- source code/transmitter/mtx/src/ui/about.h
+++ source code/transmitter/mtx/src/ui/about.h
@@ -7,6 +7,7 @@
  Notes:-
  - The text should be brief and on point; there is limited flash memory.
  - There is no need to meticulously style or format the text; it automatically resizes to fit the screen.
+ - Line feed and form feed can as well be used. Carriage return is ignored.
  - Do not remove or delete the copyright notices, as long as the code uses the relevant components.
 */
 
@@ -18,18 +19,17 @@ static const char versionText[] PROGMEM =
 
 static const char thirdPartyNoticesText[] PROGMEM =
   "The software uses, with permissions, code from the following open source libraries: "
-  "\n"
-  "\nAdafruit GFX library"
+  "\f"
+  "Adafruit GFX library"
   "\nCopyright (c) 2013 Adafruit Industries"
-  "\n"
-  "\nSparkFun External EEPROM Arduino library"
+  "\f"
+  "SparkFun External EEPROM Arduino library"
   "\nCopyright (c) 2016 SparkFun Electronics"
-  "\n"
-  "\nArduino-Lora library"
+  "\f"
+  "Arduino-Lora library"
   "\nCopyright (c) 2016 Sandeep Mistry"
-  "\n"
-  "\n"
-  "\nSpecial thanks to all the contributors to the Arduino core "
+  "\f"
+  "Special thanks to all the contributors to the Arduino core "
   "and standard libraries that this software makes use of.";
 
 static const char disclaimerText[] PROGMEM = 
@@ -38,6 +38,7 @@ static const char disclaimerText[] PROGMEM =
   "for any direct, indirect, incidental, special, exemplary or consequential "
   "damages (including but not limited to personal and/or property damage) or "
   "other liability arising from the use of the software. "
-  "\n\n\nImproperly operating RC models can cause serious injury or death. ";
+  "\f"
+  "Improperly operating RC models can cause serious injury or death.";
 
 #endif
