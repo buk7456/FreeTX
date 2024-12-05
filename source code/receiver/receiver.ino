1fabf55eee02f225611a2ae39e02e22d084f4fc2
Buk <buk7456@gmail.com>
2024-12-05 17:06:37 +0300

    Update receiver.ino

--- source code/receiver/receiver.ino
+++ source code/receiver/receiver.ino
@@ -2,6 +2,11 @@
   Firmware for the rc receiver.
   Target mcu: Atmega328p
   Compiles on Arduino 1.8.x or later
+  Use compiler optimization option -O3 if possible.
+
+  Notes:
+  - If modifying the source code, do not use large delays inside functions that are called within the main loop; 
+    rather write the source code in a non-blocking fashion.
   
   Authors:
   Buk7456 (original author)
