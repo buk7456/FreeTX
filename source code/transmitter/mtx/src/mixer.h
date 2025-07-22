81f489d8a3d8a00b0e3939b200e11a2ed59945d0
Buk <buk7456@gmail.com>
2025-07-22 19:52:21 +0300

    Update mixer.h

--- source code/transmitter/mtx/src/mixer.h
+++ source code/transmitter/mtx/src/mixer.h
@@ -4,6 +4,9 @@
 void computeChannelOutputs();
 
 int16_t calcRateExpo(int16_t input, int16_t rate, int16_t expo);
+int16_t calcExpo(int16_t input, int16_t expo);
+int16_t weightAndOffset(int16_t input, int16_t weight, int16_t offset);
+int16_t calcDifferential(int16_t input, int16_t diff);
 bool checkSwitchCondition(uint8_t sw);
 void moveMix(uint8_t newPos, uint8_t oldPos);
 void swapMix(uint8_t posA, uint8_t posB);
@@ -13,5 +16,9 @@ void reinitialiseMixerCalculations();
 int16_t adjustTrim(uint8_t idx, int16_t val, uint8_t incButton, uint8_t decButton);
 
 extern int16_t mixSources[MIX_SOURCES_COUNT]; 
+
+extern uint8_t focusedMixIdx;
+extern int16_t focusedMixInputVal;
+extern int16_t focusedMixOutputVal;
 
 #endif
