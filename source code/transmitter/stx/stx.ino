424ff6e4f0e192c5aa4b471db3608a83d9dfa092
Buk <buk7456@gmail.com>
2025-12-26 15:24:01 +0300

    Update stx.ino

--- source code/transmitter/stx/stx.ino
+++ source code/transmitter/stx/stx.ino
@@ -2,6 +2,7 @@
   Firmware for the RC transmitter.
   Target MCU: ATmega328P
   Compiles on Arduino 1.8.x or later
+  Use compiler optimization option -O2 or -O3 if possible.
   
   Authors:
   Buk7456 (original author)
